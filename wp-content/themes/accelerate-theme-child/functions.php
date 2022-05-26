d2a024052dc3b5e7428b427f47717dc0114d6c07
Georgina-Watt <georginak.watt@gmail.com>
2022-05-26 11:19:56 +0100

    Custom Post Types

--- wp-content/themes/accelerate-theme-child/functions.php
+++ wp-content/themes/accelerate-theme-child/functions.php
@@ -9,7 +9,22 @@
 * @subpackage Accelerate Marketing
 * @since Accelerate Marketing 2.0
 */
-
+//Custom post types function 
+function create_custom_post_types() {
+	//create a case study custom post
+    register_post_type( 'case_studies',
+        array(
+            'labels' => array(
+                'name' => __( 'Case Studies' ),
+                'singular_name' => __( 'Case Study' )
+            ),
+            'public' => true,
+            'has_archive' => true,
+            'rewrite' => array( 'slug' => 'case-studies' ),
+        )
+    );
+}
+add_action( 'init', 'create_custom_post_types' );
 // Enqueue scripts and styles
 function accelerate_child_scripts(){
 	wp_enqueue_style( 'accelerate-style', get_template_directory_uri() . '/style.css' );
